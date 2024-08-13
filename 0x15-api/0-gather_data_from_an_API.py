c7b43741918a25c5fb5b415c9dcca40172c618ff
Alaa <aemm7682@gmail.com>
2024-08-13 21:35:34 +0300

    0-1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,21 +16,20 @@ if __name__ == "__main__":
     data = result_1.json()
     result_2 = get('https://jsonplaceholder.typicode.com/users/')
     data2 = result_2.json()
-    print(data)
-    # for i in data2:
-    #     if i.get('id') == int(argv[1]):
-    #         employee = i.get('name')
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
 
-    # for i in data:
-    #     if i.get('userId') == int(argv[1]):
-    #         total += 1
+    for i in data:
+        if i.get('userId') == int(argv[1]):
+            total += 1
 
-    #         if i.get('completed') is True:
-    #             comp_job += 1
-    #             task.append(i.get('title'))
+            if i.get('completed') is True:
+                comp_job += 1
+                task.append(i.get('title'))
 
-    # print("Employee {} is done with tasks({}/{}):".format(employee, comp_job,
-    #                                                       total))
+    print("Employee {} is done with tasks({}/{}):".format(employee, comp_job,
+                                                          total))
 
-    # for i in task:
-    #     print("\t {}".format(i))
+    for i in task:
+        print("\t {}".format(i))
